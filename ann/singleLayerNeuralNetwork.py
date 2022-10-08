bd4d6d666398040d4c1b2ca31ba308a0747179f3
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-10-08 23:06:34 +0100

    complate the back propagation algorithm and tests

--- ann/singleLayerNeuralNetwork.py
+++ ann/singleLayerNeuralNetwork.py
@@ -4,20 +4,23 @@ import numpy as np
 
 class SingleLayerNeuralNetwork(object):
     
-    def __init__(self,iterations=1,activation_function=None,weights_interval=[-0.5,0.5]):
+    def __init__(self,iterations=1,activation_function=None,weights_interval=[0,1],baise=0.5,alpha=0.1):
         self.iterations = iterations
         self.activation_function = activation_function
         self.low = weights_interval[0]
         self.high = weights_interval[1]
         
+        self.alpha = alpha
+        self.baise = baise
+        
     def _eval_(self,X,w):
         return np.sum(np.multiply(X,w))
     
-    def _adjust_(self):
-        return
-    
+    def _adjust_(self,X,y,output):    
+        self.weights += self.alpha*(y-output)*X
+            
     def _feed_forward_(self,X,w):
-        return self.activation_function(self._eval(X,w))
+        return self.activation_function(self._eval_(X,w))
     
     def _SLNN_(self,X,y):
         
@@ -33,23 +36,27 @@ class SingleLayerNeuralNetwork(object):
                 self.errors[i] = y[i] - self.output[i]
                 
                 if self.errors[i] != 0:
-                    self.weights = self._back_propagation_(X.iloc[i],y.iloc[i],self.output[i])
+                    self._back_propagation_(X.iloc[i],y.iloc[i],self.output[i])
                     
             self.iterations -= 1
         
         return self.output
     
-    def _back_propagation_(self,X,y,output):
+    def _back_propagation_(self,X,y,output):  
+        self._adjust_(X,y,output)    
         
-        # update the weights
-        
-        self._adjust_()
-        
-        return       
+    def weights(self):
+        return self.weights
     
     def fit(self,X,y):
         return self._SLNN_(X,y)
             
     def predict(self,X):
-        return self._feed_forward_(X,self.weights)
+        
+        self.predictions = np.zeros(shape=(X.shape[0]))
+                
+        for i in range(X.shape[0]):
+            self.predictions[i] = self._feed_forward_(X.iloc[i],self.weights)
+        
+        return self.predictions
     
