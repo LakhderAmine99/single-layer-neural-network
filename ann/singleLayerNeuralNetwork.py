6c1866a8579fc10c47fd9154ef78017bdbd37689
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-10-09 13:19:32 +0100

    add the biais to the neural network

--- ann/singleLayerNeuralNetwork.py
+++ ann/singleLayerNeuralNetwork.py
@@ -4,27 +4,27 @@ import numpy as np
 
 class SingleLayerNeuralNetwork(object):
     
-    def __init__(self,iterations=1,activation_function=None,weights_interval=[0,1],baise=0.5,alpha=0.1):
+    def __init__(self,iterations=1,activation_function=None,weights_interval=[0,1],biais=0.5,alpha=0.1):
         self.iterations = iterations
         self.activation_function = activation_function
         self.low = weights_interval[0]
         self.high = weights_interval[1]
         
         self.alpha = alpha
-        self.baise = baise
+        self.biais = biais
         
     def _eval_(self,X,w):
-        return np.sum(np.multiply(X,w))
+        return np.sum(np.multiply(X,w)) + self.biais
     
     def _adjust_(self,X,y,output):    
         self.weights += self.alpha*(y-output)*X
+        self.biais += self.alpha*(y-output)
             
     def _feed_forward_(self,X,w):
         return self.activation_function(self._eval_(X,w))
     
     def _SLNN_(self,X,y):
         
-        self.errors = np.zeros(shape=(X.shape[0]))
         self.output = np.zeros(shape=(X.shape[0]))
         self.weights = np.random.randint(low=self.low*10,high=self.high*10,size=(X.shape[1]))/10
 
@@ -33,9 +33,9 @@ class SingleLayerNeuralNetwork(object):
             for i in range(X.shape[0]):
 
                 self.output[i] = self._feed_forward_(X.iloc[i],self.weights)
-                self.errors[i] = y[i] - self.output[i]
+                error = y[i] - self.output[i]
                 
-                if self.errors[i] != 0:
+                if error != 0:
                     self._back_propagation_(X.iloc[i],y.iloc[i],self.output[i])
                     
             self.iterations -= 1
